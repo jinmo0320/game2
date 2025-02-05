41c9155cd874811c36b132f1e8f05cc13faab768
jinmo0320 <jinmo040320@gmail.com>
2025-02-05 16:19:15 +0900

    firebase 작동안되는 상태

--- public/firebase.js
+++
@@ -1,8 +0,0 @@
-async function setUp() {
-  const firebaseConfig = await fetch("../firebase_config.json").then((res) =>
-    res.json()
-  );
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-}
-setUp();
